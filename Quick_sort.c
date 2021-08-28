08ddfb6e76e15c65c15989715ff6083fb2fe133d
Pratham Choudhary <prathamchoudhary23@gmail.com>
2021-08-28 10:50:18 +0530

    Added comments

--- Quick_sort.c
+++ Quick_sort.c
@@ -17,7 +17,13 @@ void Swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
-
+/*  
+   Time Complexity- 
+    best case- O(nlogn)----when the middle of the array is a pivot.
+    worst case-O(n^2)----when the middle is not a pivot .for n elements (n-1)
+    times the partition needs to be done.
+    average case-O(nlogn).    
+*/
 int partition(int arr[], int low, int high)
 {
     int pivot, i, j;
